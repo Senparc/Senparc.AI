bd56caeb585e3f31ffdaff21f4813085296b782e
苏震巍 <zsu@senparc.com>
2023-05-07 23:29:10 +0800

    整理 Keys 相关类

---
+++ src/Senparc.AI/Entities/Keys/NeuCharOpenAIKeys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Senparc.AI
+{
+    public class NeuCharOpenAIKeys
+    {
+        public string ApiKey { get; set; }
+        public string NeuCharEndpoint { get; set; }
+        /// <summary>
+        /// NeuCharOpenAIApiVersion，固定值
+        /// </summary>
+        public string NeuCharOpenAIApiVersion { get; set; } = "2022-12-01";
+    }
+}
