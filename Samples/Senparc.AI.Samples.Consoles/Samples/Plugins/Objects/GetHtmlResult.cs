20a1d43f542ea34ae0d79e66f32c25a817bcfa55
苏震巍 <zsu@senparc.com>
2024-05-07 00:11:17 +0800

    添加 Sample 中的 Function 示例

---
+++ Samples/Senparc.AI.Samples.Consoles/Samples/Plugins/Objects/GetHtmlResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.AI.Samples.Consoles.Samples.Plugins
+{
+
+    [Serializable]
+    public class GetHtmlResult
+    {
+        public double CostMS { get; set; }
+        public string HTML { get; set; }
+        public string Url { get; set; }
+    }
+}
