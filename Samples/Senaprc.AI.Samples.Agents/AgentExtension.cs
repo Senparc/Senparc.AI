3f349e1e74b8c37e97a72931c8e0bd61e1dc9054
苏震巍 <zsu@senparc.com>
2024-05-16 22:54:07 +0800

    添加 Agents 内容

---
+++ Samples/Senaprc.AI.Samples.Agents/AgentExtension.cs
@@ -0,0 +1,22 @@
+﻿using AutoGen.Core;
+using AutoGen.OpenAI;
+using AutoGen.SemanticKernel;
+
+namespace  Senparc.AI.Samples.Agents;
+
+public static class AgentExtension
+{
+    //public static MiddlewareStreamingAgent<OpenAIChatAgent> RegisterMessageConnector(this OpenAIChatAgent agent)
+    //{
+    //    var messageConnector = new OpenAIChatRequestMessageConnector();
+    //    return agent.RegisterStreamingMiddleware(messageConnector)
+    //        .RegisterMiddleware(messageConnector);
+    //}
+
+    //public static MiddlewareStreamingAgent<SemanticKernelAgent> RegisterMessageConnector(this SemanticKernelAgent agent)
+    //{
+    //    var messageConnector = new SemanticKernelChatMessageContentConnector();
+    //    return agent.RegisterStreamingMiddleware(messageConnector)
+    //        .RegisterMiddleware(messageConnector);
+    //}
+}
