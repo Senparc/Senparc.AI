b1b87878601a0433a5e65a577d1e18eb5c9928f3
苏震巍 <zsu@senparc.com>
2024-05-22 00:14:44 +0800

    v0.16.1 将 ISenparcAiSetting.ModelName、DeploymentName 从属性改为 public

--- Samples/Senaprc.AI.Samples.Agents/SampleHelper.cs
+++ Samples/Senaprc.AI.Samples.Agents/SampleHelper.cs
@@ -8,12 +8,6 @@ namespace Senparc.AI.Samples.Agents
 {
     public static class SampleHelper
     {
-        //public static string Default_Chat_ModeName = "gpt-35-turbo";//"chatglm2";//"gpt-35-turbo";//gpt-4-1106
-        //public static string Default_TextCompletion_ModeName = "text-davinci-003";//gpt-4-1106
-        //public static string Default_TextEmbedding_ModeName = "text-embedding-ada-002";
-        //public static string Default_TextCompletion_ModeName = "chatglm2";
-        //public static string Default_TextEmbedding_ModeName = "chatglm2";
-
         /// <summary>
         /// Get AppSettings file name.
         /// </summary>
